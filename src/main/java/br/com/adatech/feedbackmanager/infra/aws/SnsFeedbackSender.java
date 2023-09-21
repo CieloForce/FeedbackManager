e38f217c382d115af1f9ad8ffffd082e977d0483
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-21 02:46:45 -0300

    Fix: Application Service is up and running.

--- src/main/java/br/com/adatech/feedbackmanager/infra/aws/SnsFeedbackSender.java
+++ src/main/java/br/com/adatech/feedbackmanager/infra/aws/SnsFeedbackSender.java
@@ -2,21 +2,20 @@ package br.com.adatech.feedbackmanager.infra.aws;
 
 import br.com.adatech.feedbackmanager.adapter.FeedbackSenderAdapter;
 import br.com.adatech.feedbackmanager.core.entity.CustomerFeedback;
-import com.amazonaws.services.sns.AmazonSNS;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SnsFeedbackSender implements FeedbackSenderAdapter {
-
-    private final AmazonSNS amazonSNS;
-
-    @Autowired
-    public SnsFeedbackSender(AmazonSNS amazonSNS){
-        this.amazonSNS = amazonSNS;
-    }
+//
+//    private final AmazonSNS amazonSNS;
+//
+//    @Autowired
+//    public SnsFeedbackSender(AmazonSNS amazonSNS){
+//        this.amazonSNS = amazonSNS;
+//    }
     @Override
     public void sendCustomerFeedback(CustomerFeedback customerFeedback) {
         //TO DO: fazer a publicação do CustomerFeedback no tópico da AWS accordingly with its type.
+        System.out.println("OK!!");
     }
 }
