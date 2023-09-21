c67fc0138f204f70125ca4b4d3ddba00d6a9f1e5
stmarcelo <stmarcelo@yahoo.com.br>
2023-09-20 21:26:22 -0300

    add comments

--- src/test/java/br/com/adatech/feedbackmanager/StartupTests.java
+++ src/test/java/br/com/adatech/feedbackmanager/StartupTests.java
@@ -2,10 +2,8 @@ package br.com.adatech.feedbackmanager;
 
 import br.com.adatech.feedbackmanager.core.entity.CustomerFeedback;
 import br.com.adatech.feedbackmanager.core.entity.FeedbackType;
-import br.com.adatech.feedbackmanager.core.entity.repository.CustomerFeedbackRepository;
-import br.com.adatech.feedbackmanager.core.entity.service.CustomerFeedbackService;
+import br.com.adatech.feedbackmanager.dao.service.CustomerFeedbackService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -17,7 +15,7 @@ class StartupTests {
 
         CustomerFeedback model = new CustomerFeedback();
         model.setMessage("Mensagem de teste");
-        model.setType(FeedbackType.Sugestoes);
+        model.setType(FeedbackType.suggestion);
 
         service.create(model);
     }
