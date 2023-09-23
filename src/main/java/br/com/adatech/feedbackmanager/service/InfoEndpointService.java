3b59aa699bf3d64b1985e50d4639d297ddf9779f
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-23 15:12:24 -0300

    Feat: GET api/info?queue= endpoint consumption up and running in sync with FIFO remotion.

--- src/main/java/br/com/adatech/feedbackmanager/service/InfoEndpointService.java
+++ src/main/java/br/com/adatech/feedbackmanager/service/InfoEndpointService.java
@@ -3,6 +3,7 @@ package br.com.adatech.feedbackmanager.service;
 import br.com.adatech.feedbackmanager.application.FeedbackReceiverService;
 import br.com.adatech.feedbackmanager.core.entity.CustomerFeedback;
 import br.com.adatech.feedbackmanager.core.entity.FeedbackType;
+import br.com.adatech.feedbackmanager.dao.service.CustomerFeedbackService;
 import br.com.adatech.feedbackmanager.infra.aws.AwsSnsConfig;
 import br.com.adatech.feedbackmanager.infra.aws.SqsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,34 +18,46 @@ import java.util.List;
 public class InfoEndpointService {
     private final FeedbackReceiverService feedbackReceiverService;
     private final SqsService sqsService;
+
+    private final CustomerFeedbackService repository;
     @Autowired
-    public InfoEndpointService(FeedbackReceiverService feedbackReceiverService, SqsService sqsService){
+    public InfoEndpointService(FeedbackReceiverService feedbackReceiverService, SqsService sqsService, CustomerFeedbackService repository){
         this.feedbackReceiverService = feedbackReceiverService;
         this.sqsService = sqsService;
+        this.repository = repository;
     }
     public String getInfo(String queue) {
         if (queue == null) {
-            return "Bem-vindo ao Painel Administrativo. Escolha um serviço e forneça os parâmetros necessários.";
+            return "Bem-vindo ao Painel Administrativo. Envie seu feedback!";
         }
-        return this.dequeueCustomerFeedbackFromSQS(queue);
+        return fromStringToJSON(dequeueCustomerFeedbackFromSQS(queue));
     }
-    //Mocked info
+
+    /** Recover (consumes) the next published CustomerFeedback from aws sqs FIFO queue, in the arrived order. **/
     public String dequeueCustomerFeedbackFromSQS(String queue){
-        //Recover the next published CustomerFeedback from each possible queue from sqs.
         System.out.println("\nObtendo queuUrl da Aws correspondente à fila: " + queue + "\n");
         String queueUrl = sqsService.getQueueUrlByFeedbackType(queue);
-        int maxMessages = 1; //Pode ser um outro requestparam... se der tempo!
-        //Por default será consumido apenas um item da SQS por vez.
-        //mas a opção desse consumo de mensagens ocorrer em batch está habilitada...
+        int maxMessages = 1; //allowing batch queue consumption in order.
         List<Message> messages = feedbackReceiverService.receiveCustomerFeedback(queueUrl,maxMessages);
 
         StringBuilder result = new StringBuilder();
 
         for(Message message : messages){
-            System.out.println("Raw Message: " + message.toString());
+            System.out.println("\nRaw Message: " + message.toString() + "\n");
             result.append("Message: ").append(message.body()).append("\n");
             feedbackReceiverService.deleteMessage(queueUrl, message.receiptHandle());
         }
+
         return result.toString();
+    }
+
+    public String fromStringToJSON(String string){
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(string);
+        }catch(JsonProcessingException e){
+            System.err.println("Ocorreu um erro ao converter as informações gerais sobre os tamanhos das filas: " + e.getMessage());
+            return null;
+        }
     }
 }
